6514de024e189c7b4399c9b45db399447147ae8e
Ivan Zashchyk <89917783+judgement1100@users.noreply.github.com>
2022-04-26 18:59:52 +0300

    .

---
+++ Pract2/Lab_2_First_App/MainWindow.xaml.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Math;
+
+namespace Lab_2_First_App
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Polygon myPolygon = new Polygon();
+        static int Radius = 30;
+        static DispatcherTimer dT = new DispatcherTimer();
+        static int PointCount = 10;
+        static PointCollection collection_points = new PointCollection();
+        static List<Ellipse> list_ellipses = new List<Ellipse>();
+        static int[] oddPointsIndex = new int[PointCount];
+
+        static int maxIeration = 100;
+        static int curIteration = 0;
+        static int iterationStep = 10;
+        static int NPopulation = 5;
+        static int[][] waysArray = new int[2 * NPopulation][];
+        static double[] distancesArray = new double[2 * NPopulation];
+        static double mutationChance = 0.7;
+        static bool needRestart = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitPoints();
+            InitPolygon();
+            InitWaysArr();
+            
+            PlotPoints();
+
+            dT = new DispatcherTimer();
+            dT.Tick += new EventHandler(OneStep);
+            dT.Interval = new TimeSpan(0, 0, 0, 0, 100);
+        }
+
+        private void OneStep()
+        {
+            MyCanvas.Children.Clear();
+            CrossPopulations();
+            curIteration++;
+            PlotPoints();
+            PlotWay(GetBestWay());
+            if (curIteration == maxIeration)
+            {
+                dT.Stop();
+            }
+        }
+
+        private void OneStep(object sender, EventArgs e)
+        {
+            for (int i = 0; i < iterationStep; i++)
+            {
+                MyCanvas.Children.Clear();
+                CrossPopulations();
+                curIteration++;
+                if (curIteration >= maxIeration)
+                {
+                    dT.Stop();
+                    TextBox2.IsEnabled = true;
+                    TextBox3.IsEnabled = true;
+                    TextBox4.IsEnabled = true;
+                    TextBox5.IsEnabled = true;
+                    TextBox6.IsEnabled = true;
+                    needRestart = true;
+                }
+                PlotPoints();
+                PlotWay(GetBestWay());
+                TextBox1.Text = curIteration.ToString();
+                TextBox7.Text = Round(GetDistance(GetBestWay()), 2).ToString();
+            }
+        }
+
+        private void PlotWay(int[] BestWayIndex)
+        {
+            PointCollection Points = new PointCollection();
+
+            for (int i = 0; i < BestWayIndex.Length; i++)
+            {
+                Points.Add(collection_points[BestWayIndex[i]]);
+            }
+
+            myPolygon.Points = Points;
+            MyCanvas.Children.Add(myPolygon);
+        }
+
+        private void CrossPopulations()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < NPopulation; i++)
+            {
+                int population1Index = rnd.Next(NPopulation);
+                int population2Index = rnd.Next(NPopulation);
+                int crossPoint = rnd.Next(1, PointCount);
+
+                int[] child = GetChild(crossPoint, waysArray[population1Index], waysArray[population2Index]);
+                waysArray[i + NPopulation] = child;
+                if (rnd.NextDouble() < mutationChance)
+                {
+                    MutateElem(ref waysArray[i + NPopulation]);
+                }
+            }
+            FillDistancesArray();
+            (waysArray, distancesArray) = SortDistancesWays(waysArray, distancesArray);
+        }
+
+        private int[] GetBestWay()
+        {
+            return waysArray[0];
+        }
+
+        private (int[][], double[]) SortDistancesWays(int[][] ways, double[] distances)
+        {
+            for (int i = 1; i < distances.Length; i++)
+            {
+                for (int j = 0; j < distances.Length - 1; j++)
+                {
+                    if (distances[j] > distances[j + 1])
+                    {
+                        double tmpDist = distances[j];
+                        distances[j] = distances[j + 1];
+                        distances[j + 1] = tmpDist;
+
+                        int[] tmpWay = ways[j];
+                        ways[j] = ways[j + 1];
+                        ways[j + 1] = tmpWay;
+                    }
+                }
+            }
+            return (ways, distances);
+        }
+
+        private void MutateElem(ref int[] elem)
+        {
+            Random rnd = new Random();
+            int i11 = rnd.Next(PointCount);
+            int i22 = rnd.Next(PointCount);
+            if (i11 < i22)
+            {
+                for (int j = 0; j <= (i22 - i11) / 2; j++)
+                {
+                    int k = elem[i11 + j];
+                    elem[i11 + j] = elem[i22 - j];
+                    elem[i22 - j] = k;
+                }
+            }
+            else
+                for (int j = 0; j <= (i11 - i22) / 2; j++)
+                {
+                    int k = elem[i22 + j];
+                    elem[i22 + j] = elem[i11 - j];
+                    elem[i11 - j] = k;
+                }
+        }
+
+        private int[] GetChild(int crossPoint, int[] population1, int[] population2)
+        {
+            Random rnd = new Random();
+            int[] part1Pop1 = new int[crossPoint];
+            int[] part2Pop1 = new int[PointCount - crossPoint];
+            Array.Copy(population1, part1Pop1, crossPoint);
+            Array.Copy(population1, crossPoint, part2Pop1, 0, population1.Length - crossPoint);
+
+            int[] part1Pop2 = new int[crossPoint];
+            int[] part2Pop2 = new int[PointCount - crossPoint];
+            Array.Copy(population2, part1Pop2, crossPoint);
+            Array.Copy(population2, crossPoint, part2Pop2, 0, population1.Length - crossPoint);
+
+            int[] temp1 = new int[population1.Length];
+            Array.Copy(part1Pop1, temp1, crossPoint);
+            Array.Copy(part2Pop2, 0, temp1, crossPoint, part2Pop1.Length);
+
+            int[] temp2 = new int[population2.Length];
+            Array.Copy(part1Pop2, temp2, crossPoint);
+            Array.Copy(part2Pop1, 0, temp2, crossPoint, part2Pop1.Length);
+
+            List<int> childList = new List<int>();
+            List<int> oddValues = new List<int>();
+
+            if (rnd.NextDouble() > 0.5)
+            {
+                for (int i = 0; i < PointCount; i++)
+                {
+                    if (!oddValues.Contains(temp1[i]))
+                    {
+                        childList.Add(temp1[i]);
+                        oddValues.Add(temp1[i]);
+                    }
+                }
+                for (int i = 0; i < PointCount; i++)
+                {
+                    if (!oddValues.Contains(temp2[i]))
+                    {
+                        childList.Add(temp2[i]);
+                        oddValues.Add(temp2[i]);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < PointCount; i++)
+                {
+                    if (!oddValues.Contains(temp2[i]))
+                    {
+                        childList.Add(temp2[i]);
+                        oddValues.Add(temp2[i]);
+                    }
+                }
+                for (int i = 0; i < PointCount; i++)
+                {
+                    if (!oddValues.Contains(temp1[i]))
+                    {
+                        childList.Add(temp1[i]);
+                        oddValues.Add(temp1[i]);
+                    }
+                }
+            }
+            
+            int[] child = childList.ToArray();
+            return child;
+        }
+
+        private void FillDistancesArray()
+        {
+            for (int i = 0; i < 2 * NPopulation; i++)
+            {
+                double dist = GetDistance(waysArray[i]);
+                distancesArray[i] = dist;
+            }
+        }
+
+        private double GetDistance(int[] way)
+        {
+            double distance = 0;
+            Point p1, p2;
+            for (int i = 0; i < way.Length - 1; i++)
+            {
+                p1 = collection_points[way[i]];
+                p2 = collection_points[way[i + 1]];
+                distance += Sqrt(Pow(p1.X - p2.X, 2) + Pow(p1.Y - p2.Y, 2));
+            }
+            p1 = collection_points[way[way.Length - 1]];
+            p2 = collection_points[way[0]];
+            distance += Sqrt(Pow(p1.X - p2.X, 2) + Pow(p1.Y - p2.Y, 2));
+            return distance;
+        }
+
+        private void InitWaysArr()
+        {
+            List<int> oddValues = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < NPopulation; i++)
+            {
+                waysArray[i] = new int[PointCount];
+                for (int j = 0; j < PointCount; j++)
+                {
+                    int num = 0;
+                    while (true)
+                    {
+                        num = rnd.Next(PointCount);
+                        if (!oddValues.Contains(num))
+                        {
+                            break;
+                        }
+                    }
+                    oddValues.Add(num);
+                    waysArray[i][j] = num;
+                }
+                oddValues.Clear();
+            }
+        }
+
+        private void InitPolygon()
+        {
+            myPolygon.Stroke = System.Windows.Media.Brushes.Red;
+            myPolygon.StrokeThickness = 1;
+        }
+
+        private void InitPoints()
+        {
+            Random rnd = new Random();
+            collection_points.Clear();
+            list_ellipses.Clear();
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Point p = new Point();
+
+                p.X = rnd.Next(Radius, (int)(MainWin.Width * 0.60 - 3 * Radius));
+                p.Y = rnd.Next(Radius, (int)(MainWin.Height * 0.98 - 3 * Radius));
+                collection_points.Add(p);
+            }
+
+            for (int i = 0; i < PointCount; i++)
+            { 
+                Ellipse el = new Ellipse();
+
+                el.StrokeThickness = 1;
+                el.Height = el.Width = Radius;
+                el.Stroke = Brushes.Black;
+                el.Fill = Brushes.LightGreen;
+                list_ellipses.Add(el); 
+            }            
+        }
+
+        private void PlotPoints()
+        {
+            MyCanvas.Children.Clear();
+            for (int i = 0; i < PointCount; i++)
+            {
+                Canvas.SetLeft(list_ellipses[i], collection_points[i].X - Radius / 2);
+                Canvas.SetTop(list_ellipses[i], collection_points[i].Y - Radius / 2);
+                MyCanvas.Children.Add(list_ellipses[i]);
+            }
+        }
+
+        private void StopStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (dT.IsEnabled)
+            {
+                if (curIteration == maxIeration)
+                {
+                    curIteration = 0;
+                    needRestart = true;
+                }
+                TextBox2.IsEnabled = true;
+                TextBox3.IsEnabled = true;
+                TextBox4.IsEnabled = true;
+                TextBox5.IsEnabled = true;
+                TextBox6.IsEnabled = true;
+                dT.Stop();
+            }
+            else
+            {
+                if (needRestart)
+                {
+                    curIteration = 0;
+                    TextBox2.IsEnabled = false;
+                    TextBox3.IsEnabled = false;
+                    TextBox4.IsEnabled = false;
+                    TextBox5.IsEnabled = false;
+                    TextBox6.IsEnabled = false;
+                    InitPoints();
+                    InitPolygon();
+                    PlotPoints();
+                    InitWaysArr();
+                }
+                needRestart = false;
+                dT.Start();
+            }
+        }
+
+        private void TextBox2_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                PointCount = Convert.ToInt32(TextBox2.Text);
+                if (PointCount > 500)
+                {
+                    PointCount = 500;
+                }
+                oddPointsIndex = new int[PointCount];
+                InitPoints();
+                InitPolygon();
+                PlotPoints();
+                InitWaysArr();
+            }
+        }
+
+        private void TextBox5_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                maxIeration = int.Parse(TextBox5.Text);
+                if (maxIeration > 1000)
+                {
+                    maxIeration = 1000;
+                }
+            }
+        }
+
+        private void TextBox6_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                iterationStep = int.Parse(TextBox6.Text);
+                if (iterationStep > 1000)
+                {
+                    iterationStep = 1000;
+                }
+            }
+        }
+
+        private void TextBox4_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                mutationChance = double.Parse(TextBox4.Text);
+                if (mutationChance > 1)
+                {
+                    mutationChance = 1;
+                }
+            }
+        }
+
+        private void TextBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                NPopulation = int.Parse(TextBox3.Text);
+                if (NPopulation > 100)
+                {
+                    NPopulation = 100;
+                }
+                waysArray = new int[2 * NPopulation][];
+                distancesArray = new double[2 * NPopulation];
+            }
+        }
+    }
+}
---
+++ Pract2/Pract 2 (p. 1)/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Math;
+
+namespace Lab_2_First_App
+{
+    public partial class MainWindow : Window
+    {
+        List<Polygon> PolygonList = new List<Polygon>();
+        static int Radius = 30;
+        static DispatcherTimer dT = new DispatcherTimer();
+        static int PointCount = 5;
+        static PointCollection collection_points = new PointCollection();
+        static List<Ellipse> list_ellipses = new List<Ellipse>();
+        static int[] oddPointsIndex = new int[PointCount];
+        static int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            InitPoints();
+            InitPolygons();
+            PlotPoints();
+
+            dT = new DispatcherTimer();
+
+            int[] bestWayIndexes = GetBestWay();
+            PutPointPairInPolygonList(bestWayIndexes);
+
+            dT.Tick += new EventHandler(PlotCut);
+            dT.Interval = new TimeSpan(0, 0, 0, 0, 500);
+        }
+
+        private void PlotCut(object sender, EventArgs e)
+        {
+            if (counter == PointCount)
+            {
+                dT.Stop();
+                NumElemCB.IsEnabled = true;
+                return;
+            }
+            MyCanvas.Children.Add(PolygonList[counter]);
+            counter++;
+        }
+
+        private void PutPointPairInPolygonList(int[] bestWayIndexes)
+        {
+            PointCollection curPoints = new PointCollection();
+            Point p1, p2;
+            for (int i = 0; i < bestWayIndexes.Length; i++)
+            {
+                curPoints.Clear();
+                if (i == bestWayIndexes.Length - 1)
+                {
+                    p1 = collection_points[bestWayIndexes[i]];
+                    curPoints.Add(p1);
+                    p2 = collection_points[bestWayIndexes[0]];
+                    curPoints.Add(p2);
+                    PointCollection copiedPoints = new PointCollection(curPoints);
+                    PolygonList[i].Points = copiedPoints;
+                }
+                else
+                {
+                    p1 = collection_points[bestWayIndexes[i]];
+                    curPoints.Add(p1);
+                    p2 = collection_points[bestWayIndexes[i + 1]];
+                    curPoints.Add(p2);
+                    PointCollection copiedPoints = new PointCollection(curPoints);
+                    PolygonList[i].Points = copiedPoints;
+                }
+            }
+        }
+
+        private void InitPolygons()
+        {
+            for (int i = 0; i < PointCount; i++)
+            {
+                Polygon myPolygon = new Polygon();
+                myPolygon.Stroke = System.Windows.Media.Brushes.Red;
+                myPolygon.StrokeThickness = 1;
+                PolygonList.Add(myPolygon);
+            }
+        }
+
+        private void InitPoints()
+        {
+            Random rnd = new Random();
+            collection_points.Clear();
+            list_ellipses.Clear();
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Point p = new Point();
+
+                p.X = rnd.Next(Radius, (int)(0.75 * MainWin.Width) - 3 * Radius);
+                p.Y = rnd.Next(Radius, (int)(0.90 * MainWin.Height - 3 * Radius));
+                collection_points.Add(p);
+            }
+
+            for (int i = 0; i < PointCount; i++)
+            {
+                Ellipse el = new Ellipse();
+
+                el.StrokeThickness = 1;
+                el.Height = el.Width = Radius;
+                el.Stroke = Brushes.Black;
+                el.Fill = Brushes.LightGreen;
+                list_ellipses.Add(el);
+            }
+        }
+
+        private void PlotPoints()
+        {
+            for (int i = 0; i < PointCount; i++)
+            {
+                Canvas.SetLeft(list_ellipses[i], collection_points[i].X - Radius / 2);
+                Canvas.SetTop(list_ellipses[i], collection_points[i].Y - Radius / 2);
+                MyCanvas.Children.Add(list_ellipses[i]);
+            }
+        }
+
+        private void StopStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (dT.IsEnabled)
+            {
+                dT.Stop();
+                NumElemCB.IsEnabled = true;
+            }
+            else
+            {
+                NumElemCB.IsEnabled = false;
+                dT.Start();
+            }
+        }
+
+        private int[] GetBestWay()
+        {
+            int minIndex = 0;
+            int[] way = new int[PointCount];
+            for (int i = 0; i < PointCount; i++)
+            {
+                minIndex = GetMinIndex(collection_points, minIndex);
+                if (i == PointCount - 1)
+                {
+                    break;
+                }
+                way[i + 1] = minIndex;
+                oddPointsIndex[i] = minIndex;
+            }
+            return way;
+        }
+
+        private int GetMinIndex(PointCollection points, int index)  //  get index of the nearest available point
+        {
+            Point p1 = collection_points[index];
+            int x1 = (int)p1.X;
+            int y1 = (int)p1.Y;
+            int x2, y2 = 0;
+            double min = int.MaxValue;
+            int minIndex = 0;
+            for (int i = 0; i < PointCount; i++)
+            {
+                if (!oddPointsIndex.Contains(i))
+                {
+                    Point p2 = collection_points[i];
+                    x2 = (int)p2.X;
+                    y2 = (int)p2.Y;
+                    double len = Sqrt(Pow(x1 - x2, 2) + Pow(y1 - y2, 2));
+                    if (len < min)
+                    {
+                        min = len;
+                        minIndex = i;
+                    }
+                }
+            }
+            return minIndex;
+        }
+
+        private void NumElemCB_DropDownClosed(object sender, EventArgs e)
+        {
+            ListBoxItem item = (ListBoxItem)NumElemCB.SelectedItem;
+            PointCount = Convert.ToInt32(item.Content);
+
+            MyCanvas.Children.Clear();
+            oddPointsIndex = new int[PointCount];
+            counter = 0;
+            Start();
+        }
+
+        private void VelCB_DropDownClosed(object sender, EventArgs e)
+        {
+            ListBoxItem item = (ListBoxItem)VelCB.SelectedItem;
+            dT.Interval = new TimeSpan(0, 0, 0, 0, Convert.ToInt16(item.Content));
+        }
+    }
+}
